81d3cd67ce54da4cf9687ce2f16ada7e8e3f655f
ElitsaDev <94979851+ElitsaDev@users.noreply.github.com>
2023-01-23 14:22:08 +0200

    scale image

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,22 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
+  let mainDiv = document.querySelector(".main");
+
+  let imageDiv = document.createElement('div');
+  imageDiv.className = ('image');
+  mainDiv.appendChild(imageDiv);
+
+  let img = document.createElement('img');
+  img.src = './images/favicon.ico';
+  imageDiv.appendChild(img);
+  
+  let image = document.querySelector('.image');
+
+  image.addEventListener('click', () => {
+        image.style = 'transform: scale(2);'
+  });
+
   const button = document.querySelector(".button");
   button.addEventListener("click", () => {
     alert("💣");
